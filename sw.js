12aefb3ebd79821b533547849cc0f15d5f1b05a1
easidyl-coder <easidyl@gmail.com>
2026-09-21 23:17:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 오프라인에서도 열리게 해 주는 파일입니다.
    화면 파일은 인터넷이 되면 항상 새 버전을 먼저 받고, 안 되면 저장해 둔 것을 씁니다. */
-const CACHE = "eng-app-v2";
-const FILES = ["./", "./index.html", "./manifest.webmanifest", "./sync.js", "./firebase-config.js",
+const CACHE = "eng-app-v3";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./sync.js", "./firebase-config.js", "./ipa.js",
   "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
